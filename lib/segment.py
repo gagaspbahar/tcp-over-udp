caf704663f46f8f7e264402bffd47df911ca0784
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2022-11-09 00:58:17 +0700

    add segmentflag

--- lib/segment.py
+++ lib/segment.py
@@ -1,18 +1,29 @@
 import struct
 
 # Constants 
-SYN_FLAG = 0b0
-ACK_FLAG = 0b0
-FIN_FLAG = 0b0
+SYN_FLAG = 0b00000010
+ACK_FLAG = 0b00001000
+FIN_FLAG = 0b00000001
+DEF_FLAG = 0b00000000
 
 class SegmentFlag:
     def __init__(self, flag : bytes):
         # Init flag variable from flag byte
-        pass
+        if flag == SYN_FLAG:
+            self.type = "syn"
+        elif flag == ACK_FLAG:
+            self.type = "ack"
+        elif flag == FIN_FLAG:
+            self.type = "fin"
 
     def get_flag_bytes(self) -> bytes:
         # Convert this object to flag in byte form
-        pass
+        if self.type == "syn":
+            return SYN_FLAG
+        elif self.type == "ack":
+            return ACK_FLAG
+        elif self.type == "fin":
+            return FIN_FLAG
 
 
 
@@ -22,12 +33,20 @@ class Segment:
     # -- Internal Function --
     def __init__(self):
         # Initalize segment
-        pass
+        self.seqNumber = 0
+        self.ackNumber = 0
+        self.checksum = 0
+        self.flag = DEF_FLAG
+        self.payload = b""
 
     def __str__(self):
         # Optional, override this method for easier print(segmentA)
         output = ""
-        output += f"{'Sequence number':24} | {self.sequence}\n"
+        output += f"{'Sequence number':24} | {self.seqNumber}\n"
+        output += f"{'Acknowledgement number':24} | {self.ackNumber}\n"
+        output += f"{'Checksum':24} | {self.checksum}\n"
+        output += f"{'Flag':24} | {self.flag}\n"
+        output += f"{'Payload':24} | {self.payload}\n"
         return output
 
     def __calculate_checksum(self) -> int:
@@ -48,7 +67,7 @@ class Segment:
 
     # -- Getter --
     def get_flag(self) -> SegmentFlag:
-        pass
+        return SegmentFlag(self.flag)
 
     def get_header(self) -> dict:
         pass
